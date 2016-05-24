9b4816d8ea23159e40d9f8f332bdba3d60b34bc7
Osamu Nakanishi <s15008@std.it-college.ac.jp>
2016-05-24 13:46:45 +0900

    create TestHelloWorld

---
+++ TestHelloWorld.java
@@ -0,0 +1,14 @@
+import static java.lang.System.out;
+
+class HelloWorld {
+    public void run() {
+        out.println( "Hello World");
+    }
+}
+
+public class TestHelloWorld {
+    public static void main( String... args) {
+        HelloWorld test = new HelloWorld();
+        test.run();
+    }
+}
