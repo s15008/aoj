910df2f45dfaf6d2d9ba4b175540b2b19b76097e
Osamu Nakanishi <s15008@std.it-college.ac.jp>
2016-05-27 05:32:01 +0900

    update TestRectangle

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 + Getting Started
     + [Hello World](https://github.com/s15008/aoj/blob/master/TestHelloWorld.java)
     + [X Cubic](https://github.com/s15008/aoj/blob/master/TestXCubic.java)
-    + Rectange
+    + [Rectangle](https://github.com/s15008/aoj/blob/master/TestRectangle.java)
     + Watch
 
 + Branch on Condition
--- TestRectangle.java
+++ TestRectangle.java
@@ -2,18 +2,26 @@ import java.util.Scanner;
 import static java.lang.System.out;
 
 class Rectangle {
-    public void run() {
-        Scanner scan = new Scanner( System.in);
-        int a = scan.nextInt();
-        int b = scan.nextInt();
+    private int area;
+    private int perimeter;
 
-        out.printf( "%d %d\n", a * b, a * 2 + b * 2  );
+    Rectangle(int height, int width) {
+        this.area = height * width;
+        this.perimeter = (height * 2) + (width * 2);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d %d", this.area, this.perimeter);
     }
 }
 
 public class TestRectangle {
-    public static void main( String... args) {
-        Rectangle test = new Rectangle();
-        test.run();
+    public static void main(String... args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+
+        out.println(new Rectangle(a, b));
     }
 }
